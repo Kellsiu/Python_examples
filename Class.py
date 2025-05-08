311a85a4912658ea71674effa28414507db3a41f
Kellysiu <landyxu2010xu@gmail.com>
2025-05-08 11:12:17 +0200

    First commit

---
+++ Class.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def description(self):
+        print('This is a normal vehicle. ')
+
+class train(Vehicle):
+    def description(self):
+        print('This is a train.')
+
+class car(Vehicle):
+     def description(self):
+        print('This is a car.')
+    def 
